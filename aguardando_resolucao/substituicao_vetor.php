79a220daae8081038ab1a9bf429017aed1b03dc2
Eder <ederpvenancio@gmail.com>
2021-10-06 18:49:26 -0300

    Resolução

--- aguardando_resolucao/substituicao_vetor.php
+++ aguardando_resolucao/substituicao_vetor.php
@@ -4,17 +4,52 @@
 // Em seguida mostre o vetor X.
 
 
-$x = [0,1,2,-3,4,-5,6,0,7,10,8];
+$x = [0,1,2,-3,4,-5,6,0,7,10];
 
-for($i = 0; $i < 10; $i++){
+ /* for($i = 0; $i < 10; $i++){
  if($x[$i] = 0 || $x[$i] < 0 ){
-       $x[$i] = 1;
+       if(in_array(0, $x)){
+         unset($xprint_r($x);
+       };
      
  };
-};
+};  */
 
-echo '<pre>';
-print_r($x);
-echo '</pre>';
+
+
+ //$b = 0;
+//$c = 1;
+
+ /* for($i = 0; $i < 10; ++$i){
+      if($x[$i] = 0 || $x[$i] < 0 ){
+           $a = $x[$i] = 1;
+      
+if(in_array(0, $x)){
+      unset($x[array_search(0,$x)]);
+}}};  */
+
+
+/* $a = 0;
+$b = 1;
+for($i = 0; $i < 10; ++$i){
+      if(in_array(0, $x)){
+            unset($x[array_search(0,$x)]);
+            array_push($x, 1);
+      }}; 
+     
+*/
+
+for($i = 0; $i < 10; ++$i){
+      if($x[$i] <=0){
+             $x[$i] = 1;
+             array_filter($x);
+      };
+};
+      
+
+
+ echo '<pre>';
+ print_r($x);
+ echo '</pre>';
 ?>
 
